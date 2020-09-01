58d4bb02539ef0dcde35677787ba9aa1d5184fec
jjpokey <35283371+jjpokey@users.noreply.github.com>
2020-08-31 17:40:20 -0700

    Add files via upload

--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
 
 public class FileDecryptor {
 	/*
@@ -26,8 +29,10 @@ public class FileDecryptor {
 	 */
 	
 	public static void main(String[] args) {
-		
-		
+		int key = 0;
+		ArrayList<Character> charList = new ArrayList<Character>();
+		String readline = "";
+		String finishedMessage = "";
 		//buffered reader reads encrypted file
 		//havent started code otherwise
 		try {
@@ -36,7 +41,9 @@ public class FileDecryptor {
 			String line = br.readLine();
 			while(line != null){
 				System.out.println(line);
+				readline = line;
 				line = br.readLine();
+				
 			}
 			
 			br.close();
@@ -47,6 +54,17 @@ public class FileDecryptor {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		String skey = JOptionPane.showInputDialog("Please Enter Decrypion Key");
+		key = Integer.parseInt(skey);
+		
+		for(int i = 0; i < readline.length(); i++) {
+			charList.add(readline.charAt(i));           //adds the relevant char to arraylist
+			char charConversion = charList.get(i);     //grabs current char and stores it
+				
+			charConversion = charConversion -= key;     //encrypts char
+			finishedMessage = finishedMessage + charConversion;   //adds encrypted char every loop until full string is returned
+		}
+		JOptionPane.showMessageDialog(null, finishedMessage);
 	}
 	
 }
