26c57b4dd6ab4aab567e4a439cd40d9dfb853737
jjpokey <35283371+jjpokey@users.noreply.github.com>
2020-08-31 17:40:47 -0700

    Add files via upload

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,6 +1,31 @@
 package _03_To_Do_List;
 
-public class ToDoList {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class ToDoList implements ActionListener {
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton addTask = new JButton();
+	JButton viewTask = new JButton();
+	JButton removeTask = new JButton();
+	JButton saveList = new JButton();
+	JButton loadList = new JButton();
+	
+	ArrayList<String> taskList = new ArrayList<String>();
+	
 	/*
 	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
 	 *
@@ -21,4 +46,94 @@ public class ToDoList {
 	 * 
 	 * When the program starts, it should automatically load the last saved file into the list. 
 	 */
+	
+	public static void main(String[] args) {
+		ToDoList tdl = new ToDoList();
+		tdl.setup();
+	}
+	
+	public void setup() {
+		
+		frame.add(panel);
+		panel.add(addTask);
+		panel.add(viewTask);
+		panel.add(removeTask);
+		panel.add(saveList);
+		panel.add(loadList);
+		addTask.addActionListener(this);
+		viewTask.addActionListener(this);
+		removeTask.addActionListener(this);
+		saveList.addActionListener(this);
+		loadList.addActionListener(this);
+		addTask.setText("Enter Task");
+		viewTask.setText("View Task");
+		removeTask.setText("Remove Task");
+		saveList.setText("Save List");
+		loadList.setText("Load List");
+		frame.setVisible(true);
+		frame.setSize(100, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton buttonPressed = (JButton)e.getSource();
+		
+		if(buttonPressed == addTask) {
+			String input = JOptionPane.showInputDialog("Please enter a task");
+			taskList.add(input);
+		}
+		if(buttonPressed == viewTask) {
+			int taskNumber = 0;
+			for(int i = 0; i < taskList.size(); i++) {
+				taskNumber++;
+				JOptionPane.showMessageDialog(null, "Task " + taskNumber + ": " + taskList.get(i));
+			}
+		}
+		if(buttonPressed == removeTask) {
+			String removeNumber = JOptionPane.showInputDialog("Please enter the task number you would like to remove...");
+			int rnumber = Integer.parseInt(removeNumber);
+			taskList.remove(rnumber - 1);
+		}
+		if(buttonPressed == saveList) {
+			try {
+				FileWriter fw = new FileWriter("src/_03_To_Do_List/savedList.txt");
+				for(int j = 0; j < taskList.size(); j++) {
+					fw.write(taskList.get(j) + "\n");
+				}
+				
+				fw.close();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+		if(buttonPressed == loadList) {
+			String readLine = "";
+			//for loop? ONLY READS ONE LINE
+			try {
+				BufferedReader br = new BufferedReader(new FileReader("src/_03_To_Do_List/savedList.txt"));
+				
+				String line = br.readLine();
+				while(line != null){
+					System.out.println(line);
+					readLine = line;
+					line = br.readLine();
+				}
+				
+				br.close();
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e2) {
+				// TODO Auto-generated catch block
+				e2.printStackTrace();
+			}
+			
+			
+		}
+		
+	}
 }
